d100977426ef3a2601bd96661e676e0bda02385e
Thib <thib@ergaster.org>
2023-10-10 10:34:01 +0200

    Add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,14 @@ export default defineConfig({
 	site: "https://docs.ergaster.org",
 	integrations: [
 		starlight({
+			head: [{
+				tag: "script",
+				attrs: {
+					src: "https://analytics.eu.umami.is/script.js",
+					"data-website-id": "7ace66fe-d496-4395-9b66-8819cbb9bbe3",
+					async: true,
+				},
+			}],
 			title: 'Ergaster Docs',
 			logo: {
 				light: './src/assets/logo.svg',
